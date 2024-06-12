98a273a1400a7d3bd5f1b603d54a8ee69022affb
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-06-13 00:42:11 +0530

    Making the fetch photo function

--- GetPlacesPhotos/app.py
+++ GetPlacesPhotos/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, send_file
 import os
 import geocoder
 import requests
 from dotenv import load_dotenv
+from io import BytesIO
 
 app = Flask(__name__)
 api_key = os.getenv('api')
@@ -55,6 +56,31 @@ def get_places_data():
   
   return render_template("search.html")
 
+@app.route("/details/<fsq_id>")
+def get_places_details(fsq_id):
+  headers = {
+    "accept": "application/json",
+    "Authorization": api_key
+  }
+
+  # Fetch photos from the Foursquare API
+  resp = requests.get(url=f"https://api.foursquare.com/v3/places/{fsq_id}/photos", headers=headers)
+  data = resp.json()
+  
+  return render_template("details.html", d=data)
+
+@app.route('/fetch-image')
+def fetch_image():
+  prefix = request.args.get('prefix')
+  suffix = request.args.get('suffix')
+  img_url = "".join([prefix, "original", suffix])
+  
+  response = requests.get(img_url)
+  if response.status_code == 200:
+      img = BytesIO(response.content)
+      return send_file(img, mimetype='image/jpeg')
+  else:
+      return "Image not found", 404
 
 if __name__ == "__main__":
   configure()
